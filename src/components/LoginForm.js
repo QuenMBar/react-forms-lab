6c533f6d1db2315f5881c65d460fad8df344618d
QuenMBar <Qbeast360@gmail.com>
2021-03-18 11:07:57 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,65 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {};
-  }
+        this.state = {
+            username: "",
+            password: "",
+        };
+    }
 
-  render() {
-    return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
-  }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    /**
+     *
+     * @param {event} e
+     */
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.username !== "" || this.state.password !== "") {
+            this.props.handleLogin(this.state);
+        }
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>
+                        Username
+                        <input
+                            onChange={this.handleChange}
+                            value={this.state.username}
+                            id="username"
+                            name="username"
+                            type="text"
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password
+                        <input
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            id="password"
+                            name="password"
+                            type="password"
+                        />
+                    </label>
+                </div>
+                <div>
+                    <button type="submit">Log in</button>
+                </div>
+            </form>
+        );
+    }
 }
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,35 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {};
-  }
+        this.state = {
+            message: "",
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
-    );
-  }
+    updateMessage = (e) => {
+        this.setState({
+            message: e.target.value,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h4>Characters Remaining: </h4>
+                <strong>Your message: {this.props.maxChars - this.state.message.length}</strong>
+                <input
+                    onChange={this.updateMessage}
+                    value={this.state.message}
+                    type="text"
+                    name="message"
+                    id="message"
+                />
+            </div>
+        );
+    }
 }
 
 export default TwitterMessage;
